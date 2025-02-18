dfe52d8ec0d098974ef0db447af87610004b1759
straiforos <stephen@traiforos.com>
2025-02-18 00:16:05 -0500

    Add Angular Material and responsive navigation styling

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { routes } from '../app.routes';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
   standalone: true
