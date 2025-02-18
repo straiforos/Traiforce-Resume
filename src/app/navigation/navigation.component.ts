b9a5d4eb402e655d331d5592ad8ae3875f1e96a2
straiforos <stephen@traiforos.com>
2025-02-18 00:22:15 -0500

    Enhance navigation component with Material Design and active route styling

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { routes } from '../app.routes';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink, MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
   standalone: true
