cf4f07006bf2008a5f3afbaee469df29f470118e
straiforos <stephen@traiforos.com>
2025-02-18 23:37:26 -0500

    Add location icon to profile section
    - Integrate FontAwesome location icon in ProfileComponent
    - Update HTML and SCSS to display location with an icon
    - Enhance visual presentation of location information

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Basics } from 'resume';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
   @Input() basics!: Basics;
+  locationIcon = faLocationDot;
 }
