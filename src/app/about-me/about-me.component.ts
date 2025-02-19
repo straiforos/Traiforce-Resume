2f85db56e7aa052a17224f7797a65395472fdada
straiforos <stephen@traiforos.com>
2025-02-18 23:06:45 -0500

    Add FontAwesome icons to social links in About Me component
    - Integrate FontAwesome for social network icons
    - Update package.json and package-lock.json with FontAwesome dependencies
    - Enhance social link styling with icons and improved hover effects
    - Create icon mapping for various social networks with fallback icon

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResumeService } from 'resume';
-import { Basics, Interest } from 'resume';
+import { Basics, Interest, Profile } from 'resume';
 import { Observable } from 'rxjs';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faMedium,
+  faStackOverflow,
+  faInstagram,
+  faFacebook,
+  faYoutube,
+  faTwitch,
+  faCodepen,
+  faDev,
+  faReddit
+} from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
@@ -15,8 +31,29 @@ export class AboutMeComponent {
   basics$: Observable<Basics>;
   interests$: Observable<Interest[]>;
 
+  // Icon mapping for social networks
+  networkIcons: { [key: string]: any } = {
+    'GitHub': faGithub,
+    'LinkedIn': faLinkedin,
+    'Twitter': faTwitter,
+    'Medium': faMedium,
+    'Stack Overflow': faStackOverflow,
+    'Instagram': faInstagram,
+    'Facebook': faFacebook,
+    'YouTube': faYoutube,
+    'Twitch': faTwitch,
+    'CodePen': faCodepen,
+    'Dev.to': faDev,
+    'Reddit': faReddit,
+    'Website': faGlobe
+  };
+
   constructor(private resumeService: ResumeService) {
     this.basics$ = this.resumeService.basics$;
     this.interests$ = this.resumeService.interests$;
+  }
+
+  getIconForNetwork(network: string): any {
+    return this.networkIcons[network] || faGlobe;
   }
 }
