2a90e362de9a67ff872a23cf81fd31a12a63a84e
straiforos <stephen@traiforos.com>
2025-02-18 21:57:25 -0500

    Enhance footer component with Material Design styling
    - Add Material Toolbar to footer
    - Implement responsive footer design
    - Use SCSS for styling with theme variables
    - Update component to be standalone

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [MatToolbarModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
 
