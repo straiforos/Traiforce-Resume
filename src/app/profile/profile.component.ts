438fea2f92ea75754a57132b375ec8f3cd73d730
straiforos <stephen@traiforos.com>
2025-02-19 20:11:03 -0500

    Added high rez image for profile, and styled to center and keep the cropped circle styling. Resume service photo src is a fall back if it fails to load using ng optimized image.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Basics } from 'resume';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
@@ -7,11 +7,22 @@ import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, NgOptimizedImage],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
   @Input() basics!: Basics;
   locationIcon = faLocationDot;
+
+  /**
+   * Handles high-rez loading failure and falls back to the jsonresume gravatar url.
+   * @param event
+   */
+  handleImageError(event: Event) {
+    const img = event.target as HTMLImageElement;
+    if (img && this.basics.image) {
+      img.src = this.basics.image;
+    }
+  }
 }
