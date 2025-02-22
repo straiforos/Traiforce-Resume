1a992d13bda4f7ed8a1f3e1f020216577ea90a81
straiforos <stephen@traiforos.com>
2025-02-22 09:11:41 -0500

    Migrate interests to material components over custom styles.

--- src/app/interests-grid/interests-grid.component.ts
+++ src/app/interests-grid/interests-grid.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { Interest, ResumeService } from 'resume';
 import { SectionHeaderComponent } from '../shared/section-header/section-header.component';
 import { Observable } from 'rxjs';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-interests-grid',
   standalone: true,
-  imports: [CommonModule, SectionHeaderComponent],
+  imports: [
+    CommonModule,
+    SectionHeaderComponent,
+    MatChipsModule,
+    MatCardModule
+  ],
   templateUrl: './interests-grid.component.html',
   styleUrls: ['./interests-grid.component.scss']
 })
