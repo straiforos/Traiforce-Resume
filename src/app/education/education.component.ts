b1d7fffc3d3cf92393db54c8f0eeff16b49fbe8d
straiforos <stephen@traiforos.com>
2025-02-22 07:51:51 -0500

    Migated education to use material card and have its own data service instead of inputs.

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Education } from 'resume';
-import { ResumeService } from 'resume';
+import { Education, ResumeService } from 'resume';
 import { Observable } from 'rxjs';
 import { SectionHeaderComponent } from '../shared/section-header/section-header.component';
 import { MatCardModule } from '@angular/material/card';
@@ -10,10 +9,16 @@ import { MatChipsModule } from '@angular/material/chips';
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [CommonModule, SectionHeaderComponent, MatCardModule, MatChipsModule],
+  imports: [
+    CommonModule,
+    SectionHeaderComponent,
+    MatCardModule,
+    MatChipsModule,
+  ],
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent {
-  @Input() education!: Education;
+  education$: Observable<Education[]> = this.resumeService.education$;
+  constructor(private readonly resumeService: ResumeService) {}
 }
